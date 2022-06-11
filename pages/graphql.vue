cb23c1c283241190ed9ed1476c0b2d8b6bea0be4
Anthony Campolo <anthony@quicknode.com>
2022-06-11 00:46:06 -0500

    graphql

---
+++ pages/graphql.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li
+      v-for="character of characters.data.characters.results"
+      v-bind:key="character.items"
+    >
+      {{ character.name }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+  const GET_CHARACTERS = `{
+    characters {
+      results {
+        name
+        id
+      }
+    }
+  }`
+
+  const { data: characters } = await useFetch(
+    'https://rickandmortyapi.com/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: GET_CHARACTERS
+      })
+    }
+  )
+</script>
