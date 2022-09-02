73e20876723e89e68583a76dcab17e9f3eaf5a89
Ziad Jeeroburkhan <ziadjkhan@gmail.com>
2022-09-02 12:57:50 +0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,9 +6,16 @@ const app = express()
 
 app.use(express.json())
 
-/** 
-* logic for our api will go here
-*/
+app.delete(`/user/:id`, async (req, res) => {
+  const { id } = req.params
+  const user = await prisma.user.delete({
+    where: {
+      id: parseInt(id),
+    },
+  })
+  res.json(user)
+})
+
 export default {
   path: '/api',
   handler: app
